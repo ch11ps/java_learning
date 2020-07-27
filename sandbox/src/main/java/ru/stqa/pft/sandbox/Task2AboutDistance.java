7abc6b442fc9ede1beea1f61e18ad0400530bd8c
Alexander.Cherkashin <sashqee@gmail.com>
2020-07-27 17:24:06 +0700

    Задание №2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2AboutDistance.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Task2AboutDistance {
+  public static void main(String[] args) {
+    Point p1 = new Point(2, 4);
+    Point p2 = new Point(4, 16);
+
+    System.out.println("Расстояние между двумя точками равно " + p1.distance(p2));
+  }
+}
